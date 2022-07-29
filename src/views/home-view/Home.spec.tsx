42491c69f129b40a9476010e492c9a0b99bab767
Selwyn Preston <Selwyn.Preston@hackney.gov.uk>
2022-07-29 15:22:26 +0100

    add new views

---
+++ src/views/home-view/Home.spec.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { render } from "@hackney/mtfh-test-utils";
+// import { screen, waitFor } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+
+import { HomeView } from "./Home";
+
+test("FinanceDetailsView renders finance details component", async () => {
+  render(<HomeView />);
+  // add tests for finance details view rendering
+});
---
+++ src/views/home-view/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Layout } from "@mtfh/common/lib/components";
+
+import "./styles.scss";
+
+export const HomeView = (): JSX.Element => {
+  return (
+    <Layout>
+      <h1 className="lbh-heading-h1">Your Component Rendered Here</h1>
+    </Layout>
+  );
+};
