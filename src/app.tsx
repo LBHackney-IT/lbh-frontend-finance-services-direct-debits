dd54c45ba6d1d361f84253c3c0371804d2c8ebe0
dag995 <72547889+dag995@users.noreply.github.com>
2022-08-08 17:26:15 +0100

    Tenant nesting fix.

--- src/app.tsx
+++ src/app.tsx
@@ -2,63 +2,52 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import * as RouteConstants from "./routes/RouteConstants";
-
-import DirectDebitCreate from "./views/DirectDebitCreate";
-import DirectDebitEdit from "./views/DirectDebitEdit";
-import DirectDebitList from "./views/DirectDebitList";
-import DirectDebitPause from "./views/DirectDebitPause";
-import DirectDebitView from "./views/DirectDebitView";
-import Home from "./views/Home";
-import Property from "./views/Property";
-import PropertyForm from "./views/PropertyForm";
-import Search from "./views/Search";
-import Tenant from "./views/Tenant";
-import TenantForm from "./views/TenantForm";
+import * as Views from "./views";
 import "./App.css";
 
 const App = (): JSX.Element => (
   <Switch>
     <Route exact path={RouteConstants.HOME}>
-      <Home />
+      <Views.Home />
     </Route>
     <Route path={`${RouteConstants.SEARCH}/:type/:search/:page`}>
-      <Search />
+      <Views.Search />
     </Route>
 
     <Route path={`${RouteConstants.DIRECTDEBIT}/:id/edit`}>
-      <DirectDebitEdit />
+      <Views.DirectDebitEdit />
     </Route>
     <Route path={`${RouteConstants.DIRECTDEBIT}/:id/pause`}>
-      <DirectDebitPause />
+      <Views.DirectDebitPause />
     </Route>
     <Route path={`${RouteConstants.DIRECTDEBIT}/:id/create`}>
-      <DirectDebitCreate />
+      <Views.DirectDebitCreate />
     </Route>
     <Route path={`${RouteConstants.DIRECTDEBIT}/:id`}>
-      <DirectDebitView />
+      <Views.DirectDebitView />
     </Route>
     <Route path={`${RouteConstants.DIRECTDEBIT}`}>
-      <DirectDebitList />
+      <Views.DirectDebitList />
     </Route>
 
     <Route path={`${RouteConstants.PROPERTY}/form/:type`}>
-      <PropertyForm />
+      <Views.PropertyForm />
     </Route>
     <Route path={`${RouteConstants.PROPERTY}/form/:type/:id`}>
-      <PropertyForm />
+      <Views.PropertyForm />
     </Route>
     <Route path={`${RouteConstants.PROPERTY}/:id`}>
-      <Property />
+      <Views.Property />
     </Route>
 
     <Route path=":type/form">
-      <TenantForm />
+      <Views.TenantForm />
     </Route>
     <Route path=":type/form/:id">
-      <TenantForm />
+      <Views.TenantForm />
     </Route>
     <Route path=":type/:id">
-      <Tenant />
+      <Views.Tenant />
     </Route>
   </Switch>
 );
--- src/views/Tenant.js
+++ src/views/Tenant.js
@@ -14,7 +14,7 @@ const Tenant = () => {
   const Type = params.type ? params.type : "Tenant";
 
   const [searching, setSearching] = useState(false);
-  const [tenant, setTenant] = useState(undefined);
+  const [tenantData, setTenant] = useState(undefined);
 
   useEffect(() => {
     const searchCall = async () => {
@@ -28,146 +28,146 @@ const Tenant = () => {
     searchCall();
   }, [TenantId]);
 
-  const TenantViewOut = () => {
-    const back = (
-      <button
-        onClick={() => history.push(-1)}
-        className="mt-0 govuk-button lbh-button lbh-button-secondary"
-      >
-        {TextReferences.TextRef.Back}
-      </button>
-    );
+  const back = (
+    <button
+      onClick={() => history.push(-1)}
+      className="mt-0 govuk-button lbh-button lbh-button-secondary"
+    >
+      {TextReferences.TextRef.Back}
+    </button>
+  );
 
-    if (searching) {
-      return (
-        <>
-          <h1>{TextReferences.Titles.Tenant}</h1>
-          {back}
-          <h4>{TextReferences.TextRef.Searching}</h4>
-        </>
-      );
-    }
-
-    if (tenant === undefined) {
-      return;
-    }
-    if (tenant === null) {
-      return <h4>{TextReferences.TextRef.NoTenantRecords}</h4>;
-    }
-
+  if (searching) {
     return (
       <>
-        <h1>
-          {TextReferences.Titles.Tenant}: {tenant.preferredTitle}{" "}
-          {tenant.preferredFirstName} {tenant.preferredSurname}
-        </h1>
-
-        <Link
-          to={`/${Type}/form/${TenantId}`}
-          className="govuk-button lbh-button mt-0"
-          style={{ marginRight: "10px" }}
-        >
-          {TextReferences.TextRef.Edit}
-        </Link>
-
+        <h1>{TextReferences.Titles.Tenant}</h1>
         {back}
-
-        {tenant.charges && (
-          <>
-            <h2>Financial</h2>
-            {descriptionList([
-              {
-                key: "Current Balance",
-                val: CurrencyFormat(tenant.charges.currentBalance),
-              },
-              {
-                key: "Weekly Total Charges",
-                val: CurrencyFormat(tenant.charges.rent),
-              },
-              {
-                key: "Service Charge",
-                val: CurrencyFormat(tenant.charges.serviceCharge),
-              },
-              { key: "Yearly Rent Debits", val: "" },
-              { key: "Housing Benefits", val: "" },
-            ])}
-          </>
-        )}
-        <h2>Tenure</h2>
-        {descriptionList([
-          { key: "Tenure ID", val: tenant.id },
-          { key: "Tenure Type", val: tenant.personTypes.join(", ") },
-          {
-            key: "Tenancy Start Date",
-            val: DateFormat(tenant.startOfTenureDate),
-          },
-          { key: "Date of Birth", val: DateFormat(tenant.dateOfBirth) },
-        ])}
-        {tenant.tenures.length && (
-          <>
-            <h2>Properties</h2>
-            <div className="table-wrap">
-              <table className="govuk-table lbh-table">
-                <thead className="govuk-table__head">
-                  <tr className="govuk-table__row">
-                    <th className="govuk-table__header">Address</th>
-                    <th className="govuk-table__header">Date</th>
-                    <th className="govuk-table__header">Current Balance</th>
-                    <th className="govuk-table__header">Tenancy Type</th>
-                    <th className="govuk-table__header"> </th>
-                  </tr>
-                </thead>
-                <tbody className="govuk-table__body">
-                  {tenant.tenures.map((tenure) => {
-                    // console.log(tenure)
-                    return (
-                      <tr className="govuk-table__row" key={tenure.id}>
-                        <td
-                          className="govuk-table__cell"
-                          style={{ maxWidth: "240px", whiteSpace: "pre-wrap" }}
-                        >
-                          <Link
-                            className="lbh-link"
-                            to={`${RouteConstants.PROPERTY}/${tenure.id}`}
-                          >
-                            {tenure.assetFullAddress}
-                          </Link>
-                        </td>
-                        <td className="govuk-table__cell">
-                          {DateFormat(tenure.startDate)} -{" "}
-                          {tenure.endDate
-                            ? DateFormat(tenure.endDate)
-                            : "Current"}
-                        </td>
-                        <td className="govuk-table__cell">
-                          {CurrencyFormat()}
-                        </td>
-                        <td className="govuk-table__cell">{tenure.type}</td>
-                        <td className="govuk-table__cell">
-                          {tenure.isActive ? (
-                            <Link
-                              className="govuk-button lbh-button lbh-button-sm mt-0"
-                              to={`${RouteConstants.DIRECTDEBIT}/${tenure.id}/create`}
-                            >
-                              {TextReferences.TextRef.AddDirectDebit}
-                            </Link>
-                          ) : (
-                            ""
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </>
-        )}
+        <h4>{TextReferences.TextRef.Searching}</h4>
       </>
     );
-  };
+  }
 
-  return <TenantViewOut />
+  if (tenantData === undefined) {
+    return <>
+      <h1>{TextReferences.Titles.Tenant}</h1>
+      {back}
+      <h4>Tenant data could not be found.</h4>
+    </>;
+  }
+    
+  if (tenantData === null) {
+    return <h4>{TextReferences.TextRef.NoTenantRecords}</h4>;
+  }
+
+  return (
+    <>
+      <h1>
+        {TextReferences.Titles.Tenant}: {tenantData.preferredTitle}{" "}
+        {tenantData.preferredFirstName} {tenantData.preferredSurname}
+      </h1>
+
+      <Link
+        to={`/${Type}/form/${TenantId}`}
+        className="govuk-button lbh-button mt-0"
+        style={{ marginRight: "10px" }}
+      >
+        {TextReferences.TextRef.Edit}
+      </Link>
+
+      {back}
+
+      {tenantData.charges && (
+        <>
+          <h2>Financial</h2>
+          {descriptionList([
+            {
+              key: "Current Balance",
+              val: CurrencyFormat(tenantData.charges.currentBalance),
+            },
+            {
+              key: "Weekly Total Charges",
+              val: CurrencyFormat(tenantData.charges.rent),
+            },
+            {
+              key: "Service Charge",
+              val: CurrencyFormat(tenantData.charges.serviceCharge),
+            },
+            { key: "Yearly Rent Debits", val: "" },
+            { key: "Housing Benefits", val: "" },
+          ])}
+        </>
+      )}
+      <h2>Tenure</h2>
+      {descriptionList([
+        { key: "Tenure ID", val: tenantData.id },
+        { key: "Tenure Type", val: tenantData.personTypes.join(", ") },
+        {
+          key: "Tenancy Start Date",
+          val: DateFormat(tenantData.startOfTenureDate),
+        },
+        { key: "Date of Birth", val: DateFormat(tenantData.dateOfBirth) },
+      ])}
+      {tenantData.tenures.length && (
+        <>
+          <h2>Properties</h2>
+          <div className="table-wrap">
+            <table className="govuk-table lbh-table">
+              <thead className="govuk-table__head">
+                <tr className="govuk-table__row">
+                  <th className="govuk-table__header">Address</th>
+                  <th className="govuk-table__header">Date</th>
+                  <th className="govuk-table__header">Current Balance</th>
+                  <th className="govuk-table__header">Tenancy Type</th>
+                  <th className="govuk-table__header"> </th>
+                </tr>
+              </thead>
+              <tbody className="govuk-table__body">
+                {tenantData.tenures.map((tenure) => {
+                  return (
+                    <tr className="govuk-table__row" key={tenure.id}>
+                      <td
+                        className="govuk-table__cell"
+                        style={{ maxWidth: "240px", whiteSpace: "pre-wrap" }}
+                      >
+                        <Link
+                          className="lbh-link"
+                          to={`${RouteConstants.PROPERTY}/${tenure.id}`}
+                        >
+                          {tenure.assetFullAddress}
+                        </Link>
+                      </td>
+                      <td className="govuk-table__cell">
+                        {DateFormat(tenure.startDate)} -{" "}
+                        {tenure.endDate
+                          ? DateFormat(tenure.endDate)
+                          : "Current"}
+                      </td>
+                      <td className="govuk-table__cell">
+                        {CurrencyFormat()}
+                      </td>
+                      <td className="govuk-table__cell">{tenure.type}</td>
+                      <td className="govuk-table__cell">
+                        {tenure.isActive ? (
+                          <Link
+                            className="govuk-button lbh-button lbh-button-sm mt-0"
+                            to={`${RouteConstants.DIRECTDEBIT}/${tenure.id}/create`}
+                          >
+                            {TextReferences.TextRef.AddDirectDebit}
+                          </Link>
+                        ) : (
+                          ""
+                        )}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
+    </>
+  );
 
 };
 
