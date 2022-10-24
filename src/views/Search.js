3e8f4fe9e1d85a1b6335b12b17281dc22102c936
dag995 <72547889+dag995@users.noreply.github.com>
2022-10-24 13:28:53 +0100

    lint fix.

--- src/views/Search.js
+++ src/views/Search.js
@@ -33,7 +33,12 @@ const Tenants = () => {
     if (status === "loading") {
       return <h4>{TextReferences.TextRef.Searching}</h4>;
     }
-    if (data === undefined || data?.results || data?.results?.persons || data.results.persons.length === 0) {
+    if (
+      data === undefined ||
+      data?.results ||
+      data?.results?.persons ||
+      data.results.persons.length === 0
+    ) {
       const searchTypeName = TextReferences[Ref].filter(
         (opt) => Type === opt.value
       );
@@ -45,7 +50,7 @@ const Tenants = () => {
       );
     }
 
-    console.log(data)
+    console.log(data);
 
     return (
       <>
@@ -60,41 +65,26 @@ const Tenants = () => {
           <tbody className="govuk-table__body">
             {data?.results?.persons.map((tenant) => {
               return (
-                <Fragment key={tenant.id}>
-                  <tr className={`govuk-table__row ${tenant.id}`}>
-                    <td className="govuk-table__cell">
-                      <Link
-                        className="lbh-link"
-                        data-cy={`tenant-search-to-single-link-${tenant.id}`}
-                        to={`/${Type}/${tenant.id}`}
-                      >
-                        <strong>
-                          {tenant.title} {tenant.preferredFirstname}{" "}
-                          {tenant.preferredSurname}
-                        </strong>
-                      </Link>
-                    </td>
-                    <td className="govuk-table__cell">
-                      {tenant.personTypes.join(" / ")} ({Type})
-                    </td>
-                    <td className="govuk-table__cell">
-                      {CurrencyFormat(tenant.totalBalance)}
-                    </td>
-                  </tr>
-                  {/* {tenant.tenures.length && tenant.tenures.map(tenure => {
-                return <tr key={tenure.id} className={`govuk-table__row ${tenure.id}`}>
-                  <td className='govuk-table__cell'>
-                    <Link 
-                      className='lbh-link' 
-                      to={`${RouterConstants.PROPERTY}/${tenure.id}`}
-                    >{tenure.assetFullAddress}</Link>
+                <tr key={tenant.id} className={`govuk-table__row ${tenant.id}`}>
+                  <td className="govuk-table__cell">
+                    <Link
+                      className="lbh-link"
+                      data-cy={`tenant-search-to-single-link-${tenant.id}`}
+                      to={`/${Type}/${tenant.id}`}
+                    >
+                      <strong>
+                        {tenant.title} {tenant.preferredFirstname}{" "}
+                        {tenant.preferredSurname}
+                      </strong>
+                    </Link>
                   </td>
-                  <td className='govuk-table__cell'>{tenure.type}</td>
-                  <td className='govuk-table__cell'>{tenure.paymentReference}</td>
-                  <td className='govuk-table__cell'>{CurrencyFormat(tenure.totalBalance)}</td>
+                  <td className="govuk-table__cell">
+                    {tenant.personTypes.join(" / ")} ({Type})
+                  </td>
+                  <td className="govuk-table__cell">
+                    {CurrencyFormat(tenant.totalBalance)}
+                  </td>
                 </tr>
-              })} */}
-                </Fragment>
               );
             })}
           </tbody>
